281c3306e4896707594ffd55015992b4ab2f5835
youssef mosbah <104242783+yousefmosbah7@users.noreply.github.com>
2023-09-15 08:37:27 +0300

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,7 @@
+void add_std(struct Student* students);
+void print_stds(struct Student* students, sint32 count);
+void find_roll(struct Student* students,sint32 count);
+void find_first(struct Student* students,uint8 arr[]);
+void delete_std(struct Student* students,sint32 count);
+void update_std(struct Student* students,sint32 number,sint32 opt);
+void course_num(struct Student *students,uint8 *ptr);
